b5c1bf69e1c01a5737ed443f2df7905138852a88
bhavesh357 <bkadam357@gmail.com>
2020-07-24 02:49:29 +0530

    REFACTORED: to add new logos

--- src/Components/MainUnit.jsx
+++ src/Components/MainUnit.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class MainUnit extends React.Component{
     render(){
         return (
-            <div className="quantity-type-main-">
+            <div className="quantity-type-main">
                 <div className="quantity-type-main-logo">
                     <svg className="icon">
                         <use xlinkHref={this.props.logo} />
