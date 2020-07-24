12a71d873b34aa67e22ed33daedc99a4ce8c8b38
bhavesh357 <bkadam357@gmail.com>
2020-07-24 21:46:34 +0530

    ADDED: dropdown arrow to select

--- src/Components/SubUnit.jsx
+++ src/Components/SubUnit.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 class SubUnit extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isTriggered: false,
+        }
+    }
+
+    handleClick(){
+        console.log("tracking select");
+        if(!this.state.isTriggered){
+            var element= document.getElementById(this.props.message.toLowerCase());
+            element.children[0].style={"display":"none"};
+            console.log(element.children[0]);
+            
+        }
+    }
+
     render(){
         var list= this.props.subUnits.map(unit => {
             return (
@@ -13,9 +30,15 @@ class SubUnit extends React.Component{
                 {this.props.message}
             </div>
             <input className="quantity-type-sub-input" />
-            <select id="subUnitOne" className="quantity-type-sub-dropdown">
+            <div  onClick={() => this.handleClick()} id={this.props.message.toLowerCase()} className="quantity-subUnit-container">  
+                {this.state.isTriggered ? 
+                    <div className="arrow-up arrow"></div>:
+                        <div className="arrow-down arrow"></div>
+            }
+            <select className="quantity-type-sub-dropdown">
                 {list}
             </select>
+            </div>
             <div className="quantity-type-main-name">
             {this.props.name}
             </div>
